1c5d5840fa3a5faf5a988aa08991f7a2ef570548
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-06-20 11:24:00 +0200

    added nav animation

--- src/components/organisms/Nav.tsx
+++ src/components/organisms/Nav.tsx
@@ -1,6 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Nav = () => {
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <nav className="sticky top-0 z-50 inset-x-0 w-full bg-white flex">
+    <nav
+      className={`fixed flex top-0 z-50 inset-x-0 w-full bg-white transition-transform duration-300 ${
+        showNav ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       <div className="py-4 px-12 text-4xl shrink font-semibold">Panzel</div>
       <div className="py-4 px-12 text-xl grow bg-black text-white flex items-center justify-between">
         <p>Family Office</p>
