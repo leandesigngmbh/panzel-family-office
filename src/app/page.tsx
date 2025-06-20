b564ea3d2724d2152f5dbc884ec204e7c3698137
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-06-20 10:32:52 +0200

    added ScrollSmoother

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import SmootherContainer from "@/components/atoms/SmootherContainer";
+import SmootherTest from "@/components/atoms/SmootherTest";
 import Agenda from "@/components/molecules/Agenda";
 import Hero from "@/components/molecules/Hero";
 import Manifest from "@/components/molecules/Manifest";
@@ -9,38 +11,44 @@ import Nav from "@/components/organisms/Nav";
 export default function Home() {
   return (
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div className="relative">
+    <>
       <Nav />
 
-      <main className="flex flex-col items-center">
-        <ShaderBackground />
+      <SmootherContainer>
+        <div className="relative">
+          <main className="flex flex-col items-center">
+            <ShaderBackground />
 
-        <Hero />
+            {/* <SmootherTest /> */}
 
-        <Overview />
+            <Hero />
 
-        {/* Placeholder Start */}
-        <div className="h-[50vh]"></div>
-        {/* Placeholder End */}
+            <Overview />
 
-        <Agenda />
+            {/* Placeholder Start */}
+            <div className="h-[50vh]"></div>
+            {/* Placeholder End */}
 
-        {/* Video Placeholder Start */}
-        <div className="w-full h-screen items-center justify-center bg-white/50 flex">
-          <span className="border border-current rounded-full p-8">
-            Video Placeholder 🎥
-          </span>
+            <Agenda />
+
+            {/* Video Placeholder Start */}
+            <div className="w-full h-screen items-center justify-center bg-white/50 flex">
+              <span className="border border-current rounded-full p-8">
+                Video Placeholder 🎥
+              </span>
+            </div>
+            {/* Video Placeholder End */}
+
+            <Manifest />
+
+            {/* Placeholder Start */}
+            <div className="h-[50vh]"></div>
+            {/* Placeholder End */}
+          </main>
+
+          <Footer />
         </div>
-        {/* Video Placeholder End */}
-
-        <Manifest />
-
-        {/* Placeholder Start */}
-        <div className="h-[50vh]"></div>
-        {/* Placeholder End */}
-      </main>
-
-      <Footer />
-    </div>
+      </SmootherContainer>
+    </>
   );
 }
---
+++ src/components/atoms/SmootherContainer.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRef } from "react";
+import { usePathname } from "next/navigation";
+import gsap from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(useGSAP);
+}
+
+const SmootherContainer = ({ children }) => {
+  const smoother = useRef(null);
+  const pathname = usePathname();
+
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+      smoother.current = ScrollSmoother.create({
+        smooth: 2,
+        effects: true,
+      });
+    },
+    {
+      dependencies: [pathname],
+      revertOnUpdate: true,
+    }
+  );
+
+  return (
+    <div id="smooth-wrapper">
+      <div id="smooth-content">{children}</div>
+    </div>
+  );
+};
+
+export default SmootherContainer;
