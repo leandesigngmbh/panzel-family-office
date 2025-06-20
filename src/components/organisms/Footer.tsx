43f5401d3ad9634ccd4c0b689d7b40f79d84ac43
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-06-20 14:20:59 +0200

    modified ui

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -13,8 +13,15 @@ const Footer = () => {
       </div>
 
       <div>
-        Panzel Family Office, Rue de Lausanne 43b, 1110 Morges, Switzerland.
-        Phone +41798444988. Email hej@panzel.ch
+        <p className="text-gray-400">
+          Copyright © 2025 Panzel Family Office is a German-Norwegian-Swedish
+          Family Owned Office in Switzerland. All rights reserved.
+        </p>
+
+        <p>
+          Panzel Family Office, Rue de Lausanne 43b, 1110 Morges, Switzerland.
+          Phone +41798444988. Email hej@panzel.ch
+        </p>
       </div>
     </footer>
   );
