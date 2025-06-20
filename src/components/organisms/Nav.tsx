01ff39086ee7dc597894a8e5d64139c71dcf9e57
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-06-20 10:36:00 +0200

    modified nav

--- src/components/organisms/Nav.tsx
+++ src/components/organisms/Nav.tsx
@@ -2,7 +2,7 @@ const Nav = () => {
   return (
     <nav className="sticky top-0 z-50 inset-x-0 w-full bg-white flex">
       <div className="py-4 px-12 text-4xl shrink font-bold">Panzel</div>
-      <div className="py-4 px-12 text-xl grow bg-gray-900 text-white flex items-center justify-between">
+      <div className="py-4 px-12 text-xl grow bg-black text-white flex items-center justify-between">
         <p>Family Office</p>
 
         <ul className="flex gap-3">
